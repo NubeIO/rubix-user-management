94f7566b12d69f6ed2d2b516c7a2cd2b55e91972
RaiBnod <binodrayee@gmail.com>
2021-06-14 23:02:33 +0545

    Return multiple configs for different sites

--- src/resources/third_party/resource_mqtt.py
+++ src/resources/third_party/resource_mqtt.py
@@ -5,6 +5,8 @@ from registry.registry import RubixRegistry
 from rubix_http.resource import RubixResource
 
 from src import AppSetting
+from src.models.user.model_user import UserModel
+from src.resources.utils import get_access_token, decode_jwt_token
 
 
 class MqttConfigResource(RubixResource):
@@ -19,7 +21,13 @@ class MqttTopicsResource(RubixResource):
     @classmethod
     def get(cls):
         global_uuid: str = RubixRegistry().read_wires_plat().get("global_uuid")
-        return {
-            "layout_topic": f'{global_uuid}/layout',
-            "alert_topic": f'{global_uuid}/alerts'
-        }
+        access_token = get_access_token()
+        uuid = decode_jwt_token(access_token).get('sub', '')
+        user = UserModel.find_by_uuid(uuid)
+        output: dict = {}
+        for site in user.sites:
+            output[site.uuid] = {
+                'layout_topic': f'{global_uuid}/{site.uuid}/layout',
+                'alert_topic': f'{global_uuid}/{site.uuid}/alerts'
+            }
+        return output
