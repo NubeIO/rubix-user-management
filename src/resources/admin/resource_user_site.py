df59dfcb87e5942075e9a0ca4e7339b768e49023
enjuthulung <enjuthulung@gmail.com>
2021-06-14 11:41:04 +0545

    Use shortuuid (uuid4 with URL supporting base57 encoding)

--- src/resources/admin/resource_user_site.py
+++ src/resources/admin/resource_user_site.py
@@ -1,4 +1,4 @@
-import uuid as uuid_
+import shortuuid
 
 from flask_restful import marshal_with, reqparse
 from rubix_http.exceptions.exception import NotFoundException
@@ -26,7 +26,7 @@ class UserSiteResourceList(RubixResource):
     @marshal_with(user_site_return_fields)
     def post(cls):
         args = cls.parser.parse_args()
-        uuid = str(uuid_.uuid4())
+        uuid = str(shortuuid.uuid())
         user_site = UserSiteModel(uuid=uuid, **args)
         user_site.save_to_db()
         return user_site
