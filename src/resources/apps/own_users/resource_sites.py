392cab377f589e7d9288208f938fdb9835a1ead1
RaiBnod <binodrayee@gmail.com>
2021-06-30 13:51:49 +0545

    Fix: find user's sites list nullability

--- src/resources/apps/own_users/resource_sites.py
+++ src/resources/apps/own_users/resource_sites.py
@@ -1,6 +1,9 @@
+from typing import List
+
 from flask_restful import marshal_with
 from rubix_http.resource import RubixResource, NotFoundException
 
+from src.models.site.model_site import SiteModel
 from src.models.user.model_user import UserModel
 from src.resources.utils import get_authorized_user_uuid
 from src.rest_schema.schema_site import site_return_fields
@@ -15,4 +18,7 @@ class SitesResourceList(RubixResource):
         user = UserModel.find_by_uuid(uuid)
         if not user:
             raise NotFoundException("User does not exist")
-        return user.sites
+        output: List[SiteModel] = []
+        for site in user.sites:
+            output.append(SiteModel.find_by_uuid(site.site_uuid))
+        return output
