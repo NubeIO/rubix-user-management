fea5fcb2d336fe3755617e39f49c88b624ceff8a
RaiBnod <binodrayee@gmail.com>
2021-09-05 22:35:04 +0545

    Fix: notification block removal issue on iteration

--- src/utils/notification.py
+++ src/utils/notification.py
@@ -9,7 +9,8 @@ from src.models.enum import Platform
 def send_fcm_notification(key: str, data: dict, platform: Platform):
     url = AppSetting.default_fcm_url
     headers = {'Content-type': 'application/json', 'Authorization': f'key={key}'}
+    data_clone = {**data}  # cloning because reference data shouldn't get updated over iteration
     if Platform.ANDROID == platform:
-        del data['notification']
-    resp = requests.post(url, data=json.dumps(data), headers=headers)
+        del data_clone['notification']
+    resp = requests.post(url, data=json.dumps(data_clone), headers=headers)
     return json.loads(resp.content)
